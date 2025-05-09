7b9dcb9b66b96f5d57b0cb9522c9f92b7a8c144e
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-05-09 12:56:17 -0400

    Unit can now playback at (a multiple of) wallclock rate. Default remains 'playback as fast as possible'.

--- src/ezmsg/xdf/source.py
+++ src/ezmsg/xdf/source.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import time
 import typing
 from dataclasses import field
 
@@ -10,6 +11,43 @@ from ezmsg.util.messages.axisarray import AxisArray
 from .iter import XDFAxisArrayIterator, XDFMultiAxArrIterator
 
 
+class PlaybackClock:
+    def __init__(
+        self,
+        rate: float = 1.0,
+        step_dur: float = 0.005,
+    ):
+        """
+        Create an object that provides a timer that can run at a specified rate,
+        and with a specified step duration.
+
+        Args:
+            rate: Speed of playback. 1.0 is real time.
+            step_dur: The duration of each step in seconds.
+                Provide the duration using the unmodified rate.
+        """
+        self._step_dur = step_dur * rate
+        self._wall_start: float = time.time() - self._step_dur / 2
+        self._step_count: int = 0
+
+    def reset(self) -> None:
+        self._wall_start = time.time() - self._step_dur / 2
+        self._step_count = 0
+
+    def _get_duration(self) -> float:
+        wall_elapsed = time.time() - self._wall_start
+        next_elapsed = self._step_count * self._step_dur
+        step_dur = max(next_elapsed - wall_elapsed, 0)
+        self._step_count += 1
+        return step_dur
+
+    async def astep(self) -> None:
+        await asyncio.sleep(self._get_duration())
+
+    def step(self) -> None:
+        time.sleep(self._get_duration())
+
+
 class XDFIteratorSettings(ez.Settings):
     filepath: typing.Union[os.PathLike, str]
     select: str
@@ -17,6 +55,7 @@ class XDFIteratorSettings(ez.Settings):
     start_time: float | None = None
     stop_time: float | None = None
     rezero: bool = True
+    playback_rate: float | None = None
     self_terminating: bool = False
     """
     If True, the unit will raise a :obj:`ez.NormalTermination` exception when the file is exhausted.
@@ -46,11 +85,17 @@ class XDFIteratorUnit(ez.Unit):
             stop_time=self.SETTINGS.stop_time,
             rezero=self.SETTINGS.rezero,
         )
+        if self.SETTINGS.playback_rate is not None:
+            self._clock = PlaybackClock(rate=self.SETTINGS.playback_rate, step_dur=self.SETTINGS.chunk_dur)
+        else:
+            self._clock = None
 
     @ez.publisher(OUTPUT_SIGNAL)
     async def pub_chunk(self) -> typing.AsyncGenerator:
         try:
             while True:
+                if self._clock is not None:
+                    await self._clock.astep()
                 msg = next(self.STATE.gen)
                 if msg.data.size > 0:
                     yield self.OUTPUT_SIGNAL, msg
@@ -90,11 +135,17 @@ class XDFMultiIteratorUnit(ez.Unit):
             rezero=self.SETTINGS.rezero,
             force_single_sample=self.SETTINGS.force_single_sample,
         )
+        if self.SETTINGS.playback_rate is not None:
+            self._clock = PlaybackClock(rate=self.SETTINGS.playback_rate, step_dur=self.SETTINGS.chunk_dur)
+        else:
+            self._clock = None
 
     @ez.publisher(OUTPUT_SIGNAL)
     async def pub_multi(self) -> typing.AsyncGenerator:
         try:
             while True:
+                if self._clock is not None:
+                    await self._clock.astep()
                 msg = next(self.STATE.gen)
                 if msg is not None:
                     yield self.OUTPUT_SIGNAL, msg
